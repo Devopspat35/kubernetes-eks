f2c08f059e6da19c45755a5a268739e7c23327ba
Pat <twtech671@gmail.com>
2025-04-09 20:22:51 -0400

    Update twtechvaultdata.sh

--- modules/Hashicorp-Vault-instance/twtechvaultdata.sh
+++ modules/Hashicorp-Vault-instance/twtechvaultdata.sh
@@ -5,26 +5,15 @@ sudo -i
 sudo hostnamectl set-hostname Hashicorp-vault
 # Step 1 - Add PGP for the package signing key. 
 sudo apt update -y 
-# install the required packages & dependencies
-sudo apt install software-properties-common passwd
-sudo apt install dnf
-# To install HashiCorp tools (e.g., Terraform, Vault, Packer) on Ubuntu, follow the official APT-based method
-# 1. Add the HashiCorp GPG key
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-# 2. Add the official HashiCorp Linux repo
+sudo apt install gpg
+# Step 2 - Add the HashiCorp GPG key.
+wget O https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg 
+# Step 3 - Verify the key's fingerprint. 
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint 
+# Step 4 - Add the official HashiCorp Linux repository. 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+# Step 5 -  install vault. 
+sudo snap install vault --classic
 
-# 3. Update and install
-sudo apt update
-sudo apt install terraform  # or vault, packer, etc.
-
-# install Hashicorp Vault on ubuntu flavor
-sudo apt -y install vault
-
-# Set Up TLS
-sudo mkdir /home/vault/
-sudo chown -R vault:vault /home/vault
-
-# Verify installation
-# vault -version
+# verify vault
+# vault --version
